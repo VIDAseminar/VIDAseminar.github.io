3a47c7695229d3479e97b78f22e00561e80512a4
Remo Shen <remoteshen@gmail.com>
2024-11-12 17:26:19 -0500

    :bug: route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home'; // Import the Home component
-import HistoryTalks from './components/HistoryTalks/HistoryTalks'; // Import the HistoryTalks component
-import Navbar from './components/Navbar/Navbar'; // Your navigation component‘
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import HistoryTalks from './components/HistoryTalks/HistoryTalks';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
