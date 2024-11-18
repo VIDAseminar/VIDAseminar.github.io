d27d9485c5670df0f39015581e23b15941e76fa2
Remo Shen <remoshen@10-18-181-23.dynapool.wireless.nyu.edu>
2024-11-18 10:06:26 -0500

    :sparkles: update info for event2

--- src/components/data.js
+++ src/components/data.js
@@ -1,42 +1,42 @@
 // src/data.js
 
 export const nextTalk = {
-  date: "Wed, Nov 6, 2024",
+  date: "Wed, Nov 20, 2024",
   time: "11:00 - 12:00",
-  location: "Room 1114, 370 Jay St",
-  speaker1: "Teal Witter",
-  topic1: "Provably Accurate Estimators for Shapley Values",
-  speaker2: "Niv Cohen",
-  topic2: "Discovering and Erasing Undesired Concepts",
+  location: "Room 1178, 370 Jay St",
+  speaker1: "Alexander Ratzan",
+  topic1: "Molecular Connectomics from Small to Large Brains",
+  speaker2: "Felipe Inagaki de Oliveira",
+  topic2: "",
 };
 
 export const talkDetails = {
-  title1: "Topic1: Provably Accurate Estimators for Shapley Values",
-  researcher1: "Teal Witter, Fifth year PhD candidate, Advised by Chris Musco and Lisa Hellerstein",
-  image1: "./images/Teal.JPG",
+  title1: "Topic1: Molecular Connectomics from Small to Large Brains",
+  researcher1: "Alexander Ratzan, Second year PhD student, Advised by Erdem Varol",
+  image1: "./images/Alex.jpg",
   abstract1:
-    "Originally developed in game theory, Shapley values are now widely used in explainable machine learning to attribute model predictions to individual input features. However, calculating Shapley values exactly can be computationally demanding, requiring an exponential number of model evaluations in the number of features. To make this process more efficient, approximation algorithms are commonly used. One of the most popular is the Kernel SHAP algorithm, known for being model-agnostic and highly effective in practice. However, Kernel SHAP lacks strong, non-asymptotic guarantees on computational complexity. To address this, we introduce Leverage SHAP, a streamlined version of Kernel SHAP that provides provably accurate Shapley value estimates with nearly linear model evaluations. Our method leverages a connection between Shapley value estimation and agnostic active learning, utilizing leverage score sampling—a powerful technique in regression. Alongside its theoretical guarantees, Leverage SHAP consistently surpasses the performance of even the optimized Kernel SHAP implementation in the widely-used SHAP library [Lundberg & Lee, 2017].",
+    "Biological neural networks, or 'connectomes', evolved under stringent evolutionary constraints to enable brains of all scales to perform complex behaviors. A longstanding goal of systems neuroscience is to uncover the genetic and molecular factors that contribute to the precise wiring of these networks. In this talk, I will present modeling approaches aimed at integrating genomics and neuroimaging to provide insight into the multimodal factors that shape the connectome across species. I will further introduce key challenges of addressing this research question, as well as avenues for maximizing impact across diverse fields from precision medicine to neuromorphic computing.",
   
-  title2: "Topic2: Discovering and Erasing Undesired Concepts",
-  researcher2: "Niv Cohen, Postdoc, Advised by Chinmay Hegde",
-  image2: "./images/Niv.png",
+  title2: "Topic2: ",
+  researcher2: "Felipe Inagaki de Oliveira, Second year PhD student, Advised by Claudio Silva",
+  image2: "",
   abstract2:
-    "The rapid growth of generative models allows an ever-increasing variety of capabilities. Yet, these models may also produce undesired content such as unsafe images, private information, or copyrighted material. In this talk, I will discuss practical methods to prevent undesired generations. First, I will show how the challenge of avoiding undesired generations manifested itself in a simple Capture-the-Flag LLM setting, where even our top defense strategy was breached. Next, I will demonstrate a similar vulnerability in state-of-the-art concept erasure methods for Text-to-Image models. Finally, I will demonstrate how Task Vectors can help mitigate these vulnerabilities.",
+    "",
 };
 
 export const upcomingTalks = [
   {
-    date: "Nov 20, 2024",
+    date: "Dec 4, 2024",
     time: "11:00 - 11:30",
-    location: "Room 1178, 370 Jay St",
-    speaker: "Alexander Ratzan",
+    location: "Room xxxx, 370 Jay St",
+    speaker: "TBD",
     topic: ""
   },
   {
-    date: "Nov 20, 2024",
+    date: "Dec 4, 2024",
     time: "11:30 - 12:00",
-    location: "Room 1178, 370 Jay St",
-    speaker: "Felipe Inagaki de Oliveira",
+    location: "Room xxxx, 370 Jay St",
+    speaker: "TBD",
     topic: "",
   }
 ];
