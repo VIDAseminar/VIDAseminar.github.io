26d602421e3d7e21e9082aa718bfe7fd3e6d6fae
Remo Shen <remoteshen@gmail.com>
2025-03-10 13:01:13 -0400

    :sparkles: mar12

--- src/components/data.js
+++ src/components/data.js
@@ -1,41 +1,42 @@
 // src/data.js
 
 export const nextTalk = {
-  date: "Wed, Dec 4, 2024",
-  time: "12:10 - 13:10",
+  date: "Wed, Mar 12, 2025",
+  time: "12:00 - 12:30",
   location: "Room 1114, 370 Jay St",
   zoomLink: "https://nyu.zoom.us/j/95901395759",
-  speaker1: "Andrew Bell",
-  topic1: "Safeguarding Large Language Models in Real-time with Tunable Safety-Performance Trade-offs",
-  speaker2: "Jewelina Wen",
-  topic2: "Understanding, Enhancing, and Augmenting Human Interaction through Adaptive Systems",
+  speaker1: "Apoorv Vikram Singh",
+  topic1:
+    "Differentially Private Synthetic Data Generation using Moment Matching",
+  // speaker2: "",
+  // topic2: "",
 };
 
 export const talkDetails = {
   title1: "Topic1: " + nextTalk.topic1,
-  researcher1: `${nextTalk.speaker1}, Ph.D. Candidate, Advised by Julia Stoyanovich`,
-  image1: "./images/Andrew.JPG",
+  researcher1: `${nextTalk.speaker1}, Ph.D. Student, Advised by Chris Musco`,
+  image1: "./images/apoorv.jpeg",
   abstract1:
-    "Large Language Models (LLMs) have been shown to be susceptible to jailbreak attacks, or adversarial attacks used to illicit high risk behavior from a model. Jailbreaks have been exploited by cybercriminals and blackhat actors to cause significant harm, highlighting the critical need to safeguard widely-deployed models. Safeguarding approaches, which include fine-tuning models or having LLMs \"self-reflect\", may lengthen the inference time of a model, incur a computational penalty, reduce the semantic fluency of an output, and restrict 'normal' model behavior. Importantly, these Safety-Performance Trade-offs (SPTs) remain an understudied area. In this work, we introduce a novel safeguard, called SafeNudge, that combines Controlled Text Generation with \"nudging,\" or using text interventions to change the behavior of a model. SafeNudge triggers during text-generation while a jailbreak attack is being executed, and can reduce successful jailbreak attempts by 30% by guiding the LLM towards a safe responses. It adds minimal latency to inference and has a negligible impact on the semantic fluency of outputs. Further, we allow for tunable SPTs. SafeNudge is open-source and available through https://pypi.org/, and is compatible with models loaded with the Hugging Face transformers library.",
+    'One of the most fundamental problems in statistics is the method of moments: given the moments of a probability distribution, the goal is to estimate the distribution (or a function of it). I will show how to do this efficiently using Chebyshev polynomials, leading to an optimal and practical algorithm. Next, I will introduce differential privacy and demonstrate how to generate differentially private synthetic data using the same approach. If time permits, I will also sketch the proof. No background in distribution learning or differential privacy is assumed. This talk is based on joint work with Cameron Musco, Christopher Musco, and Lucas Rosenblatt.',
 
-  title2: "Topic2: " + nextTalk.topic2,
-  researcher2: `${nextTalk.speaker2}, M.S. Student, Advised by Claudio Silva`,
-  image2: "./images/jewelina.JPG",
-  abstract2:
-    "By leveraging computational methods, physiological sensing, and AI-driven adaptation, I develop intelligent interfaces that dynamically respond to users' cognitive and behavioral states, optimizing experience and performance in real time. My work spans aviation, driving assistance, healthcare, and gaming, demonstrating the impact of adaptive systems across diverse domains.",
+  // title2: "Topic2: " + nextTalk.topic2,
+  // researcher2: `${nextTalk.speaker2}, M.S. Student, Advised by Claudio Silva`,
+  // image2: "./images/jewelina.JPG",
+  // abstract2:
+  //   "By leveraging computational methods, physiological sensing, and AI-driven adaptation, I develop intelligent interfaces that dynamically respond to users' cognitive and behavioral states, optimizing experience and performance in real time. My work spans aviation, driving assistance, healthcare, and gaming, demonstrating the impact of adaptive systems across diverse domains.",
 };
 
 export const upcomingTalks = [
   {
-    date: "Mar 12, 2025",
-    time: "11:00 - 11:30",
+    date: "Apr 2, 2025",
+    time: "12:00 - 12:30",
     location: "Room 1114, 370 Jay St",
     speaker: "TBD",
     topic: "",
   },
   {
     date: "Mar 12, 2025",
-    time: "11:30 - 12:00",
+    time: "12:30 - 13:00",
     location: "Room 1114, 370 Jay St",
     speaker: "TBD",
     topic: "",
@@ -99,31 +100,35 @@ export const historyTalksData = [
           "https://drive.google.com/file/d/1MaszoGCBtldtvtHKSde9U8fVBTXbmeXH/view?usp=drive_link",
       },
       {
-          name: "Aécio Santos",
-          title: "Sampling-Based Sketching Methods for Correlated Data Discovery",
-          zoomLink: "https://drive.google.com/file/d/1iNOJLoltkFRHgMf3QNYkiM_tLLVt8aDB/view?usp=drive_link",
-          slidesLink: ""
-      }
-  ]
-},
-  // Additional events...
+        name: "Aécio Santos",
+        title: "Sampling-Based Sketching Methods for Correlated Data Discovery",
+        zoomLink:
+          "https://drive.google.com/file/d/1iNOJLoltkFRHgMf3QNYkiM_tLLVt8aDB/view?usp=drive_link",
+        slidesLink: "",
+      },
+    ],
+  },
   {
     date: "Feb 26, 2025",
     speakers: [
       {
         name: "Andrew Bell",
-        title: "Safeguarding Large Language Models in Real-time with Tunable Safety-Performance Trade-offs",
+        title:
+          "Safeguarding Large Language Models in Real-time with Tunable Safety-Performance Trade-offs",
+        zoomLink:
+          "https://drive.google.com/file/d/1sj2UKfmXvQJAJvR7_uTzw1T7FUsvt4hm/view?usp=sharing",
+        slidesLink: "",
+      },
+      {
+        name: "Jewelina Wen",
+        title:
+          "Understanding, Enhancing, and Augmenting Human Interaction through Adaptive Systems",
         zoomLink:
           "https://drive.google.com/file/d/1sj2UKfmXvQJAJvR7_uTzw1T7FUsvt4hm/view?usp=sharing",
         slidesLink:
-          "",
+          "https://docs.google.com/presentation/d/1JO3gsuysW5UAIDfT-2Ryde6GyALQ5lyt/edit?usp=sharing&ouid=103225470520735592499&rtpof=true&sd=true",
       },
-      {
-          name: "Jewelina Wen",
-          title: "Understanding, Enhancing, and Augmenting Human Interaction through Adaptive Systems",
-          zoomLink: "https://drive.google.com/file/d/1sj2UKfmXvQJAJvR7_uTzw1T7FUsvt4hm/view?usp=sharing",
-          slidesLink: "https://docs.google.com/presentation/d/1JO3gsuysW5UAIDfT-2Ryde6GyALQ5lyt/edit?usp=sharing&ouid=103225470520735592499&rtpof=true&sd=true"
-      }
-  ]
-},
+    ],
+  },
+  // Additional events...
 ];
